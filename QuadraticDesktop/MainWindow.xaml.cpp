4dda4d85b7caf638d409754befb98d7a0128a459
TaylorSumers <suvorint@gmail.com>
2026-08-30 06:30:38 +0300

    MainWindow refactor

--- QuadraticDesktop/MainWindow.xaml.cpp
+++ QuadraticDesktop/MainWindow.xaml.cpp
@@ -7,11 +7,10 @@
 #include "MainWindow.g.cpp"
 #endif
 
-using namespace winrt;
-using namespace Microsoft::UI::Xaml;
-using namespace Microsoft::UI::Xaml::Controls;
-using namespace Microsoft::Web::WebView2::Core;
-using namespace Windows::Foundation;
+using namespace winrt::Microsoft::UI::Xaml;
+using namespace winrt::Microsoft::UI::Xaml::Controls;
+using namespace winrt::Microsoft::Web::WebView2::Core;
+using namespace winrt::Windows::Foundation;
 using namespace winrt::QuadraticDesktop;
 
 namespace winrt::QuadraticDesktop::implementation
@@ -34,8 +33,10 @@ namespace winrt::QuadraticDesktop::implementation
       co_return;
     }
 
-    ShowResult(Interop::Solve(a, b, c));
-    UpdateGraph(a, b, c);
+
+    Interop::Solution solution = Interop::Solve(a, b, c);
+    co_await ShowSolutionAsync(solution);
+    UpdateGraph(a, b, c, solution);
     ShowGraphInfo(a, b, c, Interop::GetGraphInfo(a, b, c));
   }
 
@@ -46,23 +47,22 @@ namespace winrt::QuadraticDesktop::implementation
 
     try {
       co_await GraphView().EnsureCoreWebView2Async();
+      CoreWebView2 core = GraphView().CoreWebView2();
 
       hstring plotlyFolder{
           winrt::Windows::ApplicationModel::Package::Current()
               .InstalledLocation()
               .Path()
-              + L"\\Assets\\Plotly"
+              + PLOTLY_RELATIVE_PATH
       };
 
-      CoreWebView2 core = GraphView().CoreWebView2();
-
       core.SetVirtualHostNameToFolderMapping(
-        L"appassets.example",
+        VIRTUAL_HOST_NAME,
         plotlyFolder,
         winrt::Microsoft::Web::WebView2::Core::
         CoreWebView2HostResourceAccessKind::DenyCors);
 
-      core.Navigate(L"https://appassets.example/graph.html");
+      core.Navigate(GRAPH_URL);
     }
     catch (winrt::hresult_error const& error) {
       initializationFailed = true;
@@ -77,127 +77,169 @@ namespace winrt::QuadraticDesktop::implementation
 
   void MainWindow::GraphView_NavigationCompleted(WebView2 const& sender, CoreWebView2NavigationCompletedEventArgs const& e)
   {
-    graphReady_ = e.IsSuccess();
+    graphData_.isReady = e.IsSuccess();
 
-    if (graphReady_ && hasGraphData_) {
+    if (graphData_.isReady && graphData_.hasData) {
       SendGraphData();
     }
   }
 
-  void MainWindow::UpdateGraph(double a, double b, double c) 
+  void MainWindow::UpdateGraph(double a, double b, double c, Interop::Solution solution) 
   {
-    graphA_ = a;
-    graphB_ = b;
-    graphC_ = c;
-    hasGraphData_ = true;
+    graphData_.coeffA = a;
+    graphData_.coeffB = b;
+    graphData_.coeffC = c;
+    graphData_.x1 = solution.x1;
+    graphData_.x2 = solution.x2;
+    graphData_.nRoots = solution.nRoots;
+    graphData_.hasData = true;
 
-    if (graphReady_) {
+    if (graphData_.isReady) {
       SendGraphData();
     }
   }
 
-  void MainWindow::ShowGraphInfo(double a, double b, double c, Interop::GraphInfo graphInfo) {
-    tbEquationString().Text(winrt::hstring{ std::format(L"y = {:.6g}x² + {:.6g}x + {:.6g}", a, b, c) });
-    brdEquationString().Visibility(Visibility::Visible);
-
-    tbVertexCords().Text(winrt::hstring{ std::format(L"({:.3g}, {:.3g})", graphInfo.vertex.x, graphInfo.vertex.y) });
-    tbAxisOfSymmetry().Text(winrt::hstring{ std::format(L"x = {:.3g}", graphInfo.vertex.x) });
-
-    winrt::hstring branchesDirectionStr{};
-    switch (graphInfo.branchesDirection) {
-      case UP:
-        branchesDirectionStr = L"вверх";
-        break;
-      case DOWN:
-        branchesDirectionStr = L"вниз";
-        break;
-      case NO_BRANCHES:
-        branchesDirectionStr = L"–";
-        tbVertexCords().Text(L"–");
-        tbAxisOfSymmetry().Text(L"–");
-        break;
-    }
-    tbBranchesDirection().Text(branchesDirectionStr);
-  }
-
-  void MainWindow::SendGraphData() 
+  void MainWindow::SendGraphData()
   {
     std::wstring json = std::format(
       LR"({{ "a":{:.17g},"b":{:.17g},"c":{:.17g}, "x1":{:.17g}, "x2":{:.17g}, "nRoots":{} }})",
-      graphA_,
-      graphB_,
-      graphC_,
-      graphX1_,
-      graphX2_,
-      graphNRoots_
+      graphData_.coeffA,
+      graphData_.coeffB,
+      graphData_.coeffC,
+      graphData_.x1,
+      graphData_.x2,
+      static_cast<int>(graphData_.nRoots)
     );
 
     GraphView().CoreWebView2().PostWebMessageAsJson(hstring{ json });
   }
 
-  fire_and_forget MainWindow::ShowResult(Interop::Solution result)
+  void MainWindow::ShowGraphInfo(double a, double b, double c, Interop::GraphInfo graphInfo)
   {
-    graphX1_ = result.x1;
-    graphX2_ = result.x2;
-    graphNRoots_ = result.nRoots;
+    ShowEquationString(a, b, c);
 
+    if (graphInfo.branchesDirection == UP || graphInfo.branchesDirection == DOWN) {
+      tbVertexCords().Text(winrt::hstring{ std::format(L"({:.3g}, {:.3g})", graphInfo.vertex.x, graphInfo.vertex.y) });
+      tbAxisOfSymmetry().Text(winrt::hstring{ std::format(L"x = {:.3g}", graphInfo.vertex.x) });
+    }
+    else {
+      tbVertexCords().Text(L"–");
+      tbAxisOfSymmetry().Text(L"–");
+    }
 
-    tbWaitCoeff().Visibility(Visibility::Collapsed);
-    grdOutput().Visibility(Visibility::Visible);
+    ShowBranchesDirection(graphInfo.branchesDirection);
+  }
+
+  IAsyncAction MainWindow::ShowSolutionAsync(Interop::Solution result)
+  {
+    ShowSolutionOutput();
 
     switch (result.nRoots) {
     case NO_ROOTS:
-      brdOneRootMsg().Visibility(Visibility::Collapsed);
-      brdTwoRootsMsg().Visibility(Visibility::Collapsed);
-      brdNoRootsMsg().Visibility(Visibility::Visible);
-      brdInfRootsMsg().Visibility(Visibility::Collapsed);
-
-      dField().Text(winrt::hstring{ std::format(L"{:.6g}", result.d) });
-      tbFirstRoot().Visibility(Visibility::Collapsed);
-      tbSecondRoot().Visibility(Visibility::Collapsed);
+      ShowNoRootsResult(result.d);
       break;
     case ONE_ROOT:
-      brdOneRootMsg().Visibility(Visibility::Visible);
-      brdTwoRootsMsg().Visibility(Visibility::Collapsed);
-      brdNoRootsMsg().Visibility(Visibility::Collapsed);
-      brdInfRootsMsg().Visibility(Visibility::Collapsed);
-
-
-      dField().Text(winrt::hstring{ std::format(L"{:.6g}", result.d) });
-      x1Field().Text(winrt::hstring{ std::format(L"{:.6g}", result.x1) });
-      tbFirstRoot().Visibility(Visibility::Visible);
-      tbSecondRoot().Visibility(Visibility::Collapsed);
+      ShowOneRootResult(result.d, result.x1);
       break;
     case TWO_ROOTS:
-      brdOneRootMsg().Visibility(Visibility::Collapsed);
-      brdTwoRootsMsg().Visibility(Visibility::Visible);
-      brdNoRootsMsg().Visibility(Visibility::Collapsed);
-      brdInfRootsMsg().Visibility(Visibility::Collapsed);
-
-      dField().Text(winrt::hstring{ std::format(L"{:.6g}", result.d) });
-      x1Field().Text(to_hstring(winrt::hstring{ std::format(L"{:.6g}", result.x1) }));
-      x2Field().Text(to_hstring(winrt::hstring{ std::format(L"{:.6g}", result.x2) }));
-      tbFirstRoot().Visibility(Visibility::Visible);
-      tbSecondRoot().Visibility(Visibility::Visible);
+      ShowTwoRootsResult(result.d, result.x1, result.x2);
       break;
     case INF_ROOTS:
-      brdOneRootMsg().Visibility(Visibility::Collapsed);
-      brdTwoRootsMsg().Visibility(Visibility::Collapsed);
-      brdNoRootsMsg().Visibility(Visibility::Collapsed);
-      brdInfRootsMsg().Visibility(Visibility::Visible);
-
-      dField().Text(winrt::hstring{ std::format(L"{:.6g}", result.d) });
-      tbFirstRoot().Visibility(Visibility::Collapsed);
-      tbSecondRoot().Visibility(Visibility::Collapsed);
+      ShowInfRootsResult(result.d);
       break;
     default:
-      tbWaitCoeff().Visibility(Visibility::Visible);
-      grdOutput().Visibility(Visibility::Collapsed);
-
-      auto errorDialog = GetErrorDialog(L"Ошибка вычисления", L"Неизвестный результат");
+      HideSolutionOutput();
+      ContentDialog errorDialog = GetErrorDialog(L"Ошибка вычисления", L"Неизвестный результат");
       co_await errorDialog.ShowAsync();
       break;
     }
+  }
+
+  void MainWindow::ShowBranchesDirection(BranchesDirection direction) 
+  {
+    winrt::hstring branchesDirectionStr{};
+    switch (direction) {
+    case UP:
+      branchesDirectionStr = L"вверх";
+      break;
+    case DOWN:
+      branchesDirectionStr = L"вниз";
+      break;
+    default:
+      branchesDirectionStr = L"–";
+      break;
+    }
+    tbBranchesDirection().Text(branchesDirectionStr);
+  }
+
+  void MainWindow::ShowEquationString(double a, double b, double c)
+  {
+    tbEquationString().Text(winrt::hstring{ std::format(L"y = {:.6g}x² + {:.6g}x + {:.6g}", a, b, c) });
+    brdEquationString().Visibility(Visibility::Visible);
+  }
+
+  void MainWindow::ShowSolutionOutput() 
+  {
+    tbWaitCoeff().Visibility(Visibility::Collapsed);
+    grdOutput().Visibility(Visibility::Visible);
+  }
+
+  void MainWindow::HideSolutionOutput() 
+  {
+    tbWaitCoeff().Visibility(Visibility::Visible);
+    grdOutput().Visibility(Visibility::Collapsed);
+  }
+
+  void MainWindow::ShowNoRootsResult(double d) 
+  {
+    brdOneRootMsg().Visibility(Visibility::Collapsed);
+    brdTwoRootsMsg().Visibility(Visibility::Collapsed);
+    brdNoRootsMsg().Visibility(Visibility::Visible);
+    brdInfRootsMsg().Visibility(Visibility::Collapsed);
+
+    dField().Text(winrt::hstring{ std::format(L"{:.6g}", d) });
+    tbFirstRoot().Visibility(Visibility::Collapsed);
+    tbSecondRoot().Visibility(Visibility::Collapsed);
+  }
+
+  void MainWindow::ShowOneRootResult(double d, double x1) 
+  {
+    brdOneRootMsg().Visibility(Visibility::Visible);
+    brdTwoRootsMsg().Visibility(Visibility::Collapsed);
+    brdNoRootsMsg().Visibility(Visibility::Collapsed);
+    brdInfRootsMsg().Visibility(Visibility::Collapsed);
+
+
+    dField().Text(winrt::hstring{ std::format(L"{:.6g}", d) });
+    x1Field().Text(winrt::hstring{ std::format(L"{:.6g}", x1) });
+    tbFirstRoot().Visibility(Visibility::Visible);
+    tbSecondRoot().Visibility(Visibility::Collapsed);
+  }
+
+  void MainWindow::ShowTwoRootsResult(double d, double x1, double x2) 
+  {
+    brdOneRootMsg().Visibility(Visibility::Collapsed);
+    brdTwoRootsMsg().Visibility(Visibility::Visible);
+    brdNoRootsMsg().Visibility(Visibility::Collapsed);
+    brdInfRootsMsg().Visibility(Visibility::Collapsed);
+
+    dField().Text(winrt::hstring{ std::format(L"{:.6g}", d) });
+    x1Field().Text(to_hstring(winrt::hstring{ std::format(L"{:.6g}", x1) }));
+    x2Field().Text(to_hstring(winrt::hstring{ std::format(L"{:.6g}", x2) }));
+    tbFirstRoot().Visibility(Visibility::Visible);
+    tbSecondRoot().Visibility(Visibility::Visible);
+  }
+
+  void MainWindow::ShowInfRootsResult(double d) 
+  {
+    brdOneRootMsg().Visibility(Visibility::Collapsed);
+    brdTwoRootsMsg().Visibility(Visibility::Collapsed);
+    brdNoRootsMsg().Visibility(Visibility::Collapsed);
+    brdInfRootsMsg().Visibility(Visibility::Visible);
+
+    dField().Text(winrt::hstring{ std::format(L"{:.6g}", d) });
+    tbFirstRoot().Visibility(Visibility::Collapsed);
+    tbSecondRoot().Visibility(Visibility::Collapsed);
   }
 
   ContentDialog MainWindow::GetErrorDialog(hstring const& header, hstring const& message)
